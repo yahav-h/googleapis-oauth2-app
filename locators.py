e61b520154b6c57c08dc99b2c8add7fd9487408f
Yahav-N-Hoffman <yahavh@avanan.com>
2022-08-01 10:54:05 +0300

    add locators module to preform user and admin flows

--- locators.py
+++ locators.py
@@ -1,17 +1,17 @@
 
 
-class OAuthUserConsentPage:
+class OAuthUserConsentTags:
     EMAIL_FIELD = ('xpath', './/*[@type="email"]')
     NEXT_BUTTON = ('xpath', './/div[@data-primary-action-label]//button[@type="button"]/span')
     PASSWORD_FIELD = ('xpath', './/*[@type="password"]')
     ALLOW_BUTTON = ('xpath', './/span[contains(text(), "Allow")]')
 
-class AdminLoginPage:
+class AdminLoginTags:
     EMAIL_FIELD = ("xpath", './/*[contains(@type, "email") or contains(@autocomplete, "username")]')
     NEXT_BUTTON = ("xpath", './/button//span[contains(text(), "Next")]')
     PASSWORD_FIELD = ("xpath", './/input[contains(@type, "password") or contains(@name, "password")]')
 
-class GoogleConsoleSecurityPage:
+class GoogleConsoleSecurityTags:
     SCROLL_TARGET = ("xpath", './/header[contains(text(), "Application-specific password")]')
     LOGIN_CHALLENGE_HEADER = ("xpath", './/header[contains(text(), "Login challenge")]')
     DISABLE_CHALLENGE_BUTTON = ("xpath", './/span[contains(text(), "Turn off for 10 mins")]')
