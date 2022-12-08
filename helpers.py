de717c8fface733a22cc1f529a056d32d2780b32
yahav-h <yahavh@avanan.com>
2022-12-08 13:11:09 +0200

    force full screen + remove duplication

--- helpers.py
+++ helpers.py
@@ -14,7 +14,7 @@ def loadmapping():
 
 def loadproperties():
     data = None
-    with open(join(getcwd(), 'resources', 'properties.yml'), 'r') as out_stream:
+    with open(join(dirname(abspath(__file__)), 'resources', 'properties.yml'), 'r') as out_stream:
         data = load(out_stream, Loader)
     return data
 
@@ -26,8 +26,8 @@ def getwebdriver():
     chrome_options = Options()
     d = DesiredCapabilities.CHROME
     chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--window-size=1920,1080")
     chrome_options.add_argument("--disable-setuid-sandbox")
-    chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--remote-debugging-port=9222")
     chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36")
     chrome_options.add_argument("--headless")
