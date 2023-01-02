d98a5cfcdab3e3f549511109c3c438d6bed8cb5b
yahav-h <yahavh@avanan.com>
2023-01-02 14:22:00 +0200

    add googleapis_Service support ; add start / stop service scripts

---
+++ googleapis_Service.py
@@ -0,0 +1,4 @@
+from googleapis_Auth import app, HOST, PORT
+
+if __name__ == '__main__':
+    app.run(host=HOST, port=PORT)
---
+++ start_service.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+./venv3/bin/python3 -m pip install --upgrade pip && ./venv3/bin/python3 -m pip install -r ./dependencies.txt
+sudo nohup ./venv3/bin/python3 ./googleapis_Service.py &
